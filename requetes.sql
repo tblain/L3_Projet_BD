4e1ff64384f0c39824dd9d34a40e5bb391716ff3
tblain <tblain69@gmail.com>
2018-12-18 10:34:10 +0100

    6eme query
    très très très longue à executer + il faudrait que je/on la reteste sur un meilleur ordi pour être vraiment sur

--- requetes.sql
+++ requetes.sql
@@ -17,4 +17,37 @@ select groupe_id, count(*)/(select count(*) from meps where groupe_id = m.groupe
 
   -- 6)
 
-select * from outcomes o1 where o1.mepid = 124851 && o1.ballotid=any(select * from outcomes o2 where mepid = 124851 && o1.vote=o2.vote);
+CREATE MATERIALIZED VIEW nb_vote_par_groupe_par_ballot
+as
+  select ballotid, groupe_id, vote, count(*)
+  from Outcomes o2
+  join Meps m2 on (o2.mepid = m2.mepid)
+  group by ballotid, groupe_id, vote;
+
+CREATE MATERIALIZED VIEW vote_majo_par_groupe_par_ballot
+as
+  select ballotid, groupe_id, vote
+  from Outcomes o
+  join Meps m on (o.mepid = m.mepid)
+  group by ballotid, groupe_id, vote
+  having count(*) >= all(
+    select count
+    from nb_vote_par_groupe_par_ballot n
+    where m.groupe_id = n.groupe_id
+      and o.ballotid = n.ballotid
+  )
+  ;
+
+  select distinct on (groupe_id) groupe_id, name_full,
+    ceil(cast(count(*) as decimal)/(select count(*) from meps join outcomes on (meps.mepid=outcomes.mepid) group by name_full having name_full = m2.name_full) * 100)
+  from Outcomes o2
+  join meps m2 on (o2.mepid = m2.mepid)
+  where groupe_id = 'ENF' and vote = any(
+    select vote
+    from vote_majo_par_groupe_par_ballot v
+    where v.ballotid = o2.ballotid
+      and v.groupe_id = m2.groupe_id
+  )
+  group by groupe_id, name_full
+  order by groupe_id, count(groupe_id) desc
+  ;
