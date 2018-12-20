cb13f66c5166b4199a07b07227635a0506e9bb8e
Mathéo Dumont <math.dumont.md@gmail.com>
2018-12-20 17:25:01 +0100

    Fact checking 1

---
+++ fact_checking.sql
@@ -0,0 +1,25 @@
+-- 1
+
+CREATE OR REPLACE FUNCTION WE_ARE_THE_OPPOSITION_PARTY(IN party TEXT) RETURNS REAL AS
+$$
+DECLARE 
+	cursor_ballot CURSOR FOR select distinct ballotid from outcomes join meps using(mepid) where national_party = party order by ballotid asc;
+	against integer;
+	-- ceux qui ont le tag
+	total_ballot integer;
+	
+BEGIN
+	against := 0;
+	select count(distinct ballotid) from outcomes join meps using(mepid) where national_party = party into total_ballot;
+  
+	
+	for line in cursor_ballot LOOP
+    IF (select majority_vote_party(line.ballotid, party) = 2) THEN
+      against := against + 1;
+    END IF;
+	END LOOP;
+	
+	return (against::decimal / total_ballot) * 100;
+END;
+$$
+LANGUAGE PLPGSQL;
